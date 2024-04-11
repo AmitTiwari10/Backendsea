b2bd7b9b749cf78215df4ed773fc136d6a67da96
Amit Tiwari <at1484746@gmail.com>
2024-04-11 12:21:38 +0530

    cors updated

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://seamedia.netlify.app"],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
