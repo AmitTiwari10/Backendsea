033492501e36c0b003d6c369bb96c40b44c568db
Amit Tiwari <at1484746@gmail.com>
2024-04-11 13:15:58 +0530

    update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,7 +6,7 @@ export const sendToken = (user, statusCode, res, message) => {
     ),
     httpOnly: true, // Set httpOnly to true
     secure:true,
-    sameSite:false
+    sameSite:"none"
   };
 
   res.status(statusCode).cookie("token", token, options).json({
